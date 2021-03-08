fe9e3d19ebfb629705ee8e6ede96b4696ce98aba
Laurits H.G <201905745@post.au.dk>
2021-03-08 10:01:23 +0100

    made debtors Property

--- DebtBook/DebtBook/ViewModels/MainWindowViewModel.cs
+++ DebtBook/DebtBook/ViewModels/MainWindowViewModel.cs
@@ -10,7 +10,7 @@ namespace DebtBook
 {
     public class MainWindowViewModel
     {
-        public ObservableCollection<Debtor> debtors;
+        public ObservableCollection<Debtor> debtors { get; set; }
 
         public MainWindowViewModel()
         {
@@ -18,20 +18,32 @@ namespace DebtBook
             debtors.Add(new Debtor("Søren"));
             debtors.Add(new Debtor("Peter"));
             debtors.Add(new Debtor("Caroline"));
-            debtors.Add(new Debtor("August"));
-            debtors.Add(new Debtor("Marie"));
 
+            addDummyData();
+
+        }
+
+
+
+
+        #region DummyData
+
+        void addDummyData()
+        {
             // Søren
-            debtors[0].AddDebt(new Debt("10/5/2020",55));
+            debtors[0].AddDebt(new Debt("10/5/2020", 55));
             debtors[0].AddDebt(new Debt("12/5/2020", 255));
             debtors[0].AddDebt(new Debt("15/5/2020", 420));
 
             // Peter
             debtors[1].AddDebt(new Debt("20/7/2020", 40));
-            debtors[1].AddDebt(new Debt("22/7/2020", 510));
             debtors[1].AddDebt(new Debt("25/7/2020", 999));
 
+            // Caroline
+            debtors[2].AddDebt(new Debt("1/9/2020", 40));
+            debtors[2].AddDebt(new Debt("2/9/2020", 10));
+            debtors[2].AddDebt(new Debt("5/9/2020", 9));
         }
-
+        #endregion
     }
 }
